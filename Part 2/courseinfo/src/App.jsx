27b37b1ae6004267ece57d11ee1b03568a38dd2c
Nilaksh2003 <nilaksh.ginilytics@gmail.com>
2023-10-23 12:00:54 +0530

    separate module

--- Part 2/courseinfo/src/App.jsx
+++ Part 2/courseinfo/src/App.jsx
@@ -1,41 +1,5 @@
-const Header =({name})=>{
-return (
-  <h2>{name}</h2>
-)
-}
-
-const Part=({part})=>{
-  return(
-    <p>
-    {part.name} {part.exercises}
-  </p>
-  )
-}
-const Content=({parts})=>{
-  return(
-    <>
-    {parts.map(tempPart=><Part key={tempPart.id} part={tempPart} />)}
-    </>
-  )
-}
-
-const Total =({parts})=>{
-  return(
-    <b>total of {parts.reduce((s, p) => s + p.exercises ,0)} exercises</b>
-  )
-}
-
-const Course=({course})=>{
-return(
-  <div>
-
-    <Header name={course.name} />
-    <Content parts={course.parts} />
-    <Total parts={course.parts} />
-  </div>
-)
-}
-
+import './Course'
+import Course from './Course'
 const App = () => {
   const courses = [
     {
