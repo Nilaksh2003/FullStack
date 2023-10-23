0edc8b9e1e1ec01aab9d70dd9fdb2d8deca91228
Nilaksh2003 <nilaksh.ginilytics@gmail.com>
2023-10-23 12:58:19 +0530

    The Phonebook Step3

--- Part 2/Phonebook/src/App.jsx
+++ Part 2/Phonebook/src/App.jsx
@@ -3,12 +3,14 @@ import { useState } from 'react'
 const App = () => {
   const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
+  const [newNumber,setNewNumber]=useState('')
   const handleFormSubmit=(event)=>{
     event.preventDefault()
-    if(!persons.includes(newName))
+    if(!persons.some(person=>person.name===newName))
     {
-      setPersons(persons.concat(newName))
+      setPersons(persons.concat({name:newName,number:newNumber}))
       setNewName('')
+      setNewNumber('')
     }
     else{
       alert(`${newName} is already added to phonebook`)
@@ -22,11 +24,14 @@ const App = () => {
           name: <input value={newName} onChange={()=>{setNewName(event.target.value)}} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={()=>{setNewNumber(event.target.value)}} />
+        </div>
+        <div>
           <button type="submit" onClick={handleFormSubmit}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person,i)=><p key={i}>{person}</p>)}
+      {persons.map((person,i)=><p key={i}>{person.name} {person.number}</p>)}
     </div>
   )
 }
