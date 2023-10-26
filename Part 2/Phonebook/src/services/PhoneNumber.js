5820f03cb59761cb510bf0e162282ef542ab322e
Nilaksh2003 <nilaksh.ginilytics@gmail.com>
2023-10-26 11:51:00 +0530

    phonebook backend step9

--- Part 2/Phonebook/src/services/PhoneNumber.js
+++ Part 2/Phonebook/src/services/PhoneNumber.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl='http://localhost:3001/persons'
+const baseUrl='/api/persons'
 const getAllPhoneNumber=()=>{
     const request=axios.get(baseUrl)
     return request.then((response)=>{
